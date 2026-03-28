9fc1b556d9748301cc8c3dcc0de1f2798be884bf
km0tt <ljaljavol@gmail.com>
2026-03-28 17:42:58 +0200

    add smth

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import { createServer } from "http"
 import path from "path"
 import { fileURLToPath } from "url"
 import { readFileSync } from "fs"
+import {Server } from "socket.io"
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -30,6 +31,27 @@ const server = createServer((req, res)=>{
             res.end("Error: Not Found")
     }
 })
+
+const io = new Server(server);
+
+io.on("connection", (socket)=>{
+    console.log(`User connected with id: ${socket.id}`)
+    let nickname = "anonim"
+
+    socket.on("new-nickname", (data)=>{
+        nickname = data
+    })
+
+    socket.on("new_message", (data)=> {
+        console.log(data)
+        io.emit("message", {
+            user: nickname,
+            message: data
+        })
+    })
+})
+
+
 
 server.listen(3000, ()=>console.log("Server on!"))
 
--- static/script.js
+++ static/script.js
@@ -1,1 +1,31 @@
-console.log("it works")
+const socket = io()
+
+let form = document.querySelector("#form")
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    let message = event.target["input"].value
+    console.log(message)
+    if (message){
+        socket.emit("new_message", message)
+        event.target.reset()
+    }
+})
+
+socket.on("message", (data)=>{
+    console.log("From server: ", data)
+    addMessage(data)
+})
+
+function addMessage(message){
+    let messageList = document.querySelector(".messages")
+    let messageItem = document.createElement("li")
+    messageItem.textContent = `${message.user}: ${message.message}`
+    messageList.appendChild(messageItem)
+    window.scrollTo(0, messageList.scrollHeight)
+}
+
+document.querySelector(".auth").addEventListener("click", ()=>{
+    let nickname = prompt("Введи своє ім'я", "Анон")
+    if(nickname) socket.emit("new-nickname", nickname)
+})
