beb59213c6913d4fee9b91fe5cd2d0da7fa766c6
km0tt <ljaljavol@gmail.com>
2026-04-25 17:43:11 +0300

    add new things

--- db.js
+++ db.js
@@ -1,52 +1,96 @@
 import { createConnection } from "mysql2";
+import { genSalt, hash, compare } from "bcrypt";
 
 let db = createConnection({
     host: "localhost",
     user: "root",
     password: "root",
-    database: "md_chat"
-}).promise()
+    database: "md_chat",  
+}).promise();
 
 export async function init() {
     try {
         await db.query(`
-    CREATE TABLE IF NOT EXISTS user(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    login VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL
-    )
+        CREATE TABLE IF NOT EXISTS user(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        login VARCHAR(100) NOT NULL UNIQUE,
+        password VARCHAR(100) NOT NULL
+        )
     `);
 
         await db.query(`
-    CREATE TABLE IF NOT EXISTS message(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    author_id INT,
-    content TEXT NOT NULL,
-    CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES user(id)
-    )`);
-        
+        CREATE TABLE IF NOT EXISTS message(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        author_id INT,
+        content TEXT NOT NULL,
+        CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES user(id)
+        )`);
+
+        // await db.query(`INSERT INTO user(login, password) VALUES("admin", "admin")`)
+        // await db.query(`INSERT INTO message(author_id, content) VALUES(1, "Hello!")`)
     } catch (error) {
-        console.log(`DB error: ${error.message}`)
-    }
-
-}
-
-export async function getMessages(){
-    let [result, fields] = await db.query("SELECT m.id, m.content, u.login as author FROM message m JOIN user u ON m.author_id = u.id")
-    console.log(result)
-    return result
-}
-
-getMessages()
-
-export async function addMessage(user_id, content){
-    try{
-        db.query("INSERT INTO message(author_id, content) VALUES(?, ?)", [user_id, content])
-    }catch(error){
-        console.log(`DB error:${error.message}`)
+        console.log(`DB error: ${error.message}`);
     }
 }
 
-// addMessage(1, "How are you")
+export async function getMessages() {
+    let [result, fields] = await db.query(
+        "SELECT m.id, m.content, u.login as author FROM message m JOIN user u ON m.author_id = u.id",
+    );
+    return result;
+}
 
-export default db
+// getMessages();
+
+export async function addMessage(user_id, content) {
+    try {
+        db.query("INSERT INTO message(author_id, content) VALUES (?, ?)", [
+            user_id,
+            content,
+        ]);
+    } catch (error) {
+        console.log(`DB error: ${error.message}`);
+    }
+}
+
+// addMessage(1, "How are you?")
+
+export async function isUserExist(login) {
+    const [result, fields] = await db.query(
+        "SELECT * FROM user WHERE login = ?",
+        [login],
+    );
+    return result.length > 0;
+}
+
+export async function addUser(login, password) {
+    try {
+        let salt = await genSalt(10);
+        let hashedPassword = await hash(password, salt);
+
+        login = login.toLowerCase();
+
+        await db.query("INSERT INTO user(login, password) VALUES(?, ?)", [
+            login,
+            hashedPassword,
+        ]);
+        return true
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+export async function  getUser(login, password){
+    let results = await db.query("SELECT * FROM user WHERE login = ?", [login])
+    if (results[0].length == 0){
+        return null
+    }
+    if(!await compare(password, results[0][0].password)){
+        return false
+    }
+    return results[0][0]
+}
+
+
+export default db;
--- server.js
+++ server.js
@@ -1,49 +1,64 @@
-import { createServer } from "http"
-import path from "path"
-import { fileURLToPath } from "url"
-import { readFileSync } from "fs"
-import {Server } from "socket.io"
-import db, {init as initDB, getMessages, addMessage} from "./db.js"
+import { createServer } from "http";
+import path from "path";
+import { fileURLToPath } from "url";
+import { readFileSync } from "fs";
+import { Server } from "socket.io"
+import db, {init as initDB, getMessages, addMessage, isUserExist, addUser, getUser} from "./db.js"
+import jwt from "jsonwebtoken"
+
+import cookie from "cookie"
 
 initDB()
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 
 
-
-const server = createServer(async (req, res)=>{
-    switch(req.url){
+const server = createServer(async (req, res) => {
+    switch (req.url) {
         case "/":
-            let indexHTMLFile = getStaticFile("index.html");
-            res.writeHead(200, {"content-type": "text/html"})
-            res.end(indexHTMLFile)
+            guarded(req, res)
+            let indexHtmlFile = getStaticFile("index.html");
+            res.writeHead(200, { "content-type": "text/html" });
+            res.end(indexHtmlFile);
             break;
         case "/register":
             if(req.method == "GET"){
-            let registerHTMLFile = getStaticFile("register.html");
-            res.writeHead(200, {"content-type": "text/html"})
-            res.end(registerHTMLFile)
+                let registerHtmlFile = getStaticFile("register.html");
+                res.writeHead(200, { "content-type": "text/html" });
+                res.end(registerHtmlFile);
             }
             else if(req.method == "POST"){
                 let data = ""
                 req.on("data", (chunk)=> data += chunk)
                 req.on("end", ()=>{
-                    console.log(data)
-                    res.end()
+                    registerUser(req, res, data)
                 })
-                res.end()
+            }
+            break;
+        case "/login":
+            if(req.method == "GET"){
+                let loginHtmlFile = getStaticFile("login.html");
+                res.writeHead(200, { "content-type": "text/html" });
+                res.end(loginHtmlFile);
+            }
+            else if(req.method == "POST"){
+                let data = ""
+                req.on("data", (chunk)=> data += chunk)
+                req.on("end", ()=>{
+                    loginUser(req, res, data)
+                })
             }
             break;
         case "/style.css":
             let styleCssFile = getStaticFile("style.css");
-            res.writeHead(200, {"content-type": "text/css"})
-            res.end(styleCssFile)
+            res.writeHead(200, { "content-type": "text/css" });
+            res.end(styleCssFile);
             break;
         case "/script.js":
             let scriptJsFile = getStaticFile("script.js");
-            res.writeHead(200, {"content-type": "application/javascript"})
-            res.end(scriptJsFile)
+            res.writeHead(200, { "content-type": "application/javascript" });
+            res.end(scriptJsFile);
             break;
         case "/messages":
             let messages = await getMessages()
@@ -51,10 +66,10 @@ const server = createServer(async (req, res)=>{
             res.end(JSON.stringify(messages))
             break
         default:
-            res.statusCode = 404
-            res.end("Error: Not Found")
+            res.statusCode = 404;
+            res.end("Error: Not Found");
     }
-})
+});
 
 const io = new Server(server);
 
@@ -62,28 +77,89 @@ io.on("connection", (socket)=>{
     console.log(`User connected with id: ${socket.id}`)
     let nickname = "anonim"
 
-    socket.on("new-nickname", (data)=>{
+    socket.on("new_nickname", (data)=>{
         nickname = data
     })
 
     socket.on("new_message", async (data)=> {
-        console.log(data)
         io.emit("message", {
             user: nickname,
             message: data
         })
         await addMessage(1, data)
     })
+
 })
 
 
+server.listen(3000, () => console.log("Server on!"));
 
-server.listen(3000, ()=>console.log("Server on!"))
-
-function getStaticFile(name){
-    let pathToFile = path.join(__dirname, "static", name)
-    let bufferFile = readFileSync(pathToFile)
-    let data = Buffer.from(bufferFile)
-    return data
+function getStaticFile(name) {
+    let pathToFile = path.join(__dirname, "static", name);
+    let bufferFile = readFileSync(pathToFile);
+    let data = Buffer.from(bufferFile);
+    return data;
 }
 
+async function registerUser(req, res, data){
+    let p = JSON.parse(data)
+    let login = p.login
+    let password = p.password
+
+    if(!login || !password){
+        res.statusCode = 400
+        res.end(JSON.stringify({error: "Empty login or password"}))
+        return
+    }
+
+    if(await isUserExist(login)){
+        res.statusCode = 400
+        res.end(JSON.stringify({error: "User already exist"}))
+        return
+    }
+
+    let result = addUser(login, password)
+    if(result){
+        res.statusCode = 201
+        res.end(JSON.stringify({status: "ok"}))
+    }else{
+        res.statusCode = 500
+        res.end(JSON.stringify({error: "Server error"}))
+    }
+
+}
+
+async function loginUser(req, res, data){
+    let info = JSON.parse(data)
+    let login = info.login
+    let password = info.password
+
+
+    let user = await getUser(login, password)
+    if(user == null){
+        res.status = 404
+        res.end("user not found")
+        return
+    }
+    if(!user){
+        res.status = 401
+        res.end("incorrect credentials")
+        return
+    }
+    let token = jwt.sign({id: user.id, login: user.login}, "abc", {expiresIn: "1h"})
+    res.status = 200
+    res.end(token)
+}
+
+function getCredentials(c = ""){
+    const cookies = cookie.parse(c)
+    const token = cookies?.token
+    if(!token) return null
+    let user = jwt.verify(token, "abc")
+    return user
+}
+
+function guarded(req, res){
+    const user = getCredentials(req.headers?.cookie)
+    console.log(user)
+}
